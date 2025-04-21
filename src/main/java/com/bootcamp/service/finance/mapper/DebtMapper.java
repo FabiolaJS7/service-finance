03b501953a951f923dad2868e7b3e7bb6e34e814
Monica Fabiola Garcia Saravia <mgarcsar@emeal.nttdata.com>
2025-04-21 00:07:22 -0500

    refactorizando mapper

--- src/main/java/com/bootcamp/service/finance/mapper/DebtMapper.java
+++ src/main/java/com/bootcamp/service/finance/mapper/DebtMapper.java
@@ -4,6 +4,7 @@ import com.bootcamp.service.finance.model.DebtModel;
 import com.bootcamp.service.finance.model.DebtRequest;
 import com.bootcamp.service.finance.model.DebtResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -11,6 +12,11 @@ public interface DebtMapper {
 
     DebtMapper INSTANCE = Mappers.getMapper(DebtMapper.class);
 
+    @Mapping(source = "maintanceAmount", target = "maintenanceAmount")
+    @Mapping(source = "totalAmountToPay", target = "totalAmountPay")
     DebtModel getDebtModelOfDebtRequest(DebtRequest debtRequest);
+
+    @Mapping(source = "maintenanceAmount", target = "maintanceAmount")
+    @Mapping(source = "totalAmountPay", target = "totalAmountToPay")
     DebtResponse getDebtResponseOfDebtModel(DebtModel debtModel);
 }
